cb9db80af0696d01dbcd4bb14d16601a2b03105c
aFloatingEcho <60680403+aFloatingEcho@users.noreply.github.com>
2024-05-16 11:08:04 -0700

    updated match.py to be similar to fields found in JSON

--- models/match.py
+++ models/match.py
@@ -1,49 +1,26 @@
-from datetime import datetime
-
 
 class Match:
     """
     The match class contains information about a chess match.
     """
-    DATE_FORMAT = "%d-%m-%Y"
 
-    def __init__(self, player1, player2, day_of_match):
+    def __init__(self, player1, player2, completed, winner):
         self.player1 = player1
         self.player2 = player2
-        self.matchPoints = 0
-
-        # Much like how player class handles dates, this line copies how it was written.
-        self._match_date = None
-        # Match date information
-        self.match_date = day_of_match
+        self.completed = completed
+        self.winner = winner
 
     def __str__(self):
-        return f"<{self.day_of_match}>"
+        match_info = str(self.player1) + " vs. " + str(self.player2)
+        return match_info
 
-    @property
-    def match_date(self):
-        """
-        Property to get the match day (string) from matchdate (datetime).
-        """
-        return self.match_date.strftime(self.DATE_FORMAT)
+    def setCompleted(self, status):
+        self.completed = status
 
-    @match_date.setter
-    def match_date(self, value):
-        """
-        Set the matchdate (datetime) from a string.
-        """
-        self.match_date = datetime.strptime(value, self.DATE_FORMAT)
-
-    def setMatchPoints(self, value):
-        """
-        Sets the value of a match.
-        """
-        self.matchPoints = value
+    def setWinner(self, winner):
+        self.winner = winner
 
     def serialize(self):
         """Serialize the match into JSON format for storage."""
-        data = {attr: getattr(self, attr) for attr in ("player1", "player2", "matchPoints")}
-        # As mentioned in player.py, we want to make sure we use
-        # the str representation of datetime in JSON.
-        data["match_date"] = self.match_date
+        data = {attr: getattr(self, attr) for attr in ("player1", "player2", "completed", "winner")}
         return data
