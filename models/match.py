2ca92cb9690d33d9b19a043b1a27927fafc18f60
aFloatingEcho <60680403+aFloatingEcho@users.noreply.github.com>
2024-05-16 10:40:37 -0700

    created match class based off player.py

---
+++ models/match.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+
+
+class Match:
+    """
+    The match class contains information about a chess match.
+    """
+    DATE_FORMAT = "%d-%m-%Y"
+
+    def __init__(self, player1, player2, day_of_match):
+        self.player1 = player1
+        self.player2 = player2
+        self.matchPoints = 0
+
+        # Much like how player class handles dates, this line copies how it was written.
+        self._match_date = None
+        # Match date information
+        self.match_date = day_of_match
+
+    def __str__(self):
+        return f"<{self.day_of_match}>"
+
+    @property
+    def match_date(self):
+        """
+        Property to get the match day (string) from matchdate (datetime).
+        """
+        return self.match_date.strftime(self.DATE_FORMAT)
+
+    @match_date.setter
+    def match_date(self, value):
+        """
+        Set the matchdate (datetime) from a string.
+        """
+        self.match_date = datetime.strptime(value, self.DATE_FORMAT)
+
+    def setMatchPoints(self, value):
+        """
+        Sets the value of a match.
+        """
+        self.matchPoints = value
