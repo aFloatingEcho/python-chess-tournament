b9cb5f08a012a7b43d5896b5fdfb1571e870acf4
aFloatingEcho <60680403+aFloatingEcho@users.noreply.github.com>
2024-05-23 08:35:36 -0700

    Created fields for Tournament based off JSON.

---
+++ models/tournament.py
@@ -0,0 +1,15 @@
+
+
+class Tournament:
+
+    def __init__(self, name, dates, venue, number_of_rounds, current_round, completed, players, finished, rounds):
+        self.name = name
+        self.dates = dates
+        self.venue = venue
+        self.number_of_rounds = number_of_rounds
+        self.current_round = current_round
+        self.completed = completed
+        self.players = players
+        self.finished = finished
+        self.rounds = rounds
+
