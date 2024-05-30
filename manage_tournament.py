eb42a90ac29bcc0c7f0432ec4f5d1eead58aba2c
aFloatingEcho <60680403+aFloatingEcho@users.noreply.github.com>
2024-05-29 23:58:50 -0700

    preliminary tournmament managing program

---
+++ manage_tournament.py
@@ -0,0 +1,35 @@
+from commands import ClubListCmd
+from screens import ClubCreate, ClubView, MainMenu, PlayerEdit, PlayerView
+
+
+class App:
+    """The main controller for the tournament management program"""
+
+    SCREENS = {
+        
+        "exit": False,
+    }
+
+    def __init__(self):
+        # We start with the list of clubs (= main menu)
+        command = ClubListCmd()
+        self.context = command()
+
+    def run(self):
+        while self.context.run:
+            # Get the screen class from the mapping
+            screen = self.SCREENS[self.context.screen]
+            try:
+                # Run the screen and get the command
+                command = screen(**self.context.kwargs).run()
+                # Run the command and get a context back
+                self.context = command()
+            except KeyboardInterrupt:
+                # Ctrl-C
+                print("Bye!")
+                self.context.run = False
+
+
+if __name__ == "__main__":
+    app = App()
+    app.run()
