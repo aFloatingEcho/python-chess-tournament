de10414ac6ec20b34d8cf56258a7bddd41677b2c
aFloatingEcho <60680403+aFloatingEcho@users.noreply.github.com>
2024-05-29 17:31:29 -0700

    preliminary match reader

---
+++ screens/matches/view.py
@@ -0,0 +1,31 @@
+from commands import NoopCmd
+
+from ..base_screen import BaseScreen
+
+class MatchView(BaseScreen):
+    """Screen displayed when viewing a Match."""
+
+    def __init__(self, players, completed, winner):
+        self.players = players
+        self.completed = completed
+        self.winner = winner
+
+    def display(self):
+        print(str(self))
+        if (self.winner is not None):
+            print("The match has a winner!")
+            print("The winner is: ", self.winner)
+        else:
+            print("The match currently has no winner.")
+        if (self.completed == True):
+            print("The match is concluded.")
+        else:
+            print("The match is ongoing.")
+    
+    def get_command(self):
+        while True:
+            print("Current match in progress:")
+            self.display(self)
+            action = self.input_string()
+            if action.upper() == "E":
+                return NoopCmd("club-view", club=self.club)
