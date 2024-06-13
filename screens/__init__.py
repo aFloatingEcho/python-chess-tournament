c9a76ad34fce79a6f5b42b73a716b9ee30971dfa
aFloatingEcho <60680403+aFloatingEcho@users.noreply.github.com>
2024-06-13 07:42:50 -0700

    added missing screen information

--- screens/__init__.py
+++ screens/__init__.py
@@ -2,5 +2,8 @@ from .clubs import ClubCreate, ClubView
 from .main_menu import MainMenu
 from .players import PlayerEdit, PlayerView
 from .main_menu_tour import MainMenuTour
+from .matches import MatchView
+from .tournament import TournamentView
 
-__all__ = ["ClubCreate", "ClubView", "MainMenu", "PlayerView", "MainMenuTour"]
+__all__ = ["ClubCreate", "ClubView", "MainMenu", "PlayerView", 
+           "MainMenuTour", "TournamentView",]
