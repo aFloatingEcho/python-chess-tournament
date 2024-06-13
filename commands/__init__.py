4bdca69bae8387dfd33e616a515294b426955bcb
aFloatingEcho <60680403+aFloatingEcho@users.noreply.github.com>
2024-06-12 18:50:00 -0700

    added missing imports for view/models

--- commands/__init__.py
+++ commands/__init__.py
@@ -3,6 +3,8 @@ from .create_club import ClubCreateCmd
 from .exit import ExitCmd
 from .noop import NoopCmd
 from .update_player import PlayerUpdateCmd
+from .tour_list import TourListCmd
+from .create_tour import TournamentCreateCmd
 
 __all__ = [
     "ClubCreateCmd",
@@ -10,4 +12,6 @@ __all__ = [
     "ClubListCmd",
     "NoopCmd",
     "PlayerUpdateCmd",
+    "TourListCmd",
+    "TournamentCreateCmd",
 ]
--- models/__init__.py
+++ models/__init__.py
@@ -3,5 +3,7 @@ from .club_manager import ClubManager
 from .player import Player
 from .tournament import Tournament
 from .match import Match
+from .tournament_manager import TournamentManager
 
-__all__ = ["Player", "ChessClub", "ClubManager", "Tournament", "Match"]
+__all__ = ["Player", "ChessClub", "ClubManager", 
+           "Tournament", "Match", "TournamentManager"]
