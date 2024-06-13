e558e68e8bafc7dc4b5d3d149613b67da36cd234
aFloatingEcho <60680403+aFloatingEcho@users.noreply.github.com>
2024-06-12 18:43:17 -0700

    created tour list matching clublist

---
+++ commands/tour_list.py
@@ -0,0 +1,12 @@
+from commands.context import Context
+from models import TournamentManager
+
+from .base import BaseCommand
+
+
+class TourListCmd(BaseCommand):
+    """Command to get the list of clubs"""
+
+    def execute(self):
+        tm = TournamentManager()
+        return Context("main-menu-tour", tour=tm.tournaments)
