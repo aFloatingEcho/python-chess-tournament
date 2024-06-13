cdd5037991a59f8458fd99ebba910edaac793f12
aFloatingEcho <60680403+aFloatingEcho@users.noreply.github.com>
2024-06-13 05:56:08 -0700

    added string function to Round

--- models/rounds.py
+++ models/rounds.py
@@ -6,6 +6,12 @@ class Round:
     def __init__(self, matches):
         self.matches = matches
 
+    def __str__(self):
+        stringToReturn = ""
+        for each in self.matches:
+            stringToReturn += (str(each) + "/n")
+        return stringToReturn
+
     def serialize(self):
         """Serialize the rounds into JSON format for storage"""
         data = {attr: getattr(self, attr) for attr in ("matches")}
