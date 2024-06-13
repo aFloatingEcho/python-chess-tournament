a3ffeb738ab0ca4061e5521c531354e124bc25a9
aFloatingEcho <60680403+aFloatingEcho@users.noreply.github.com>
2024-06-13 09:05:44 -0700

    preliminary round system

---
+++ screens/rounds/view.py
@@ -0,0 +1,33 @@
+from commands import NoopCmd
+
+from ..base_screen import BaseScreen
+
+
+class ViewRound(BaseScreen):
+    """Screen displayed when viewing a round."""
+
+    def __init__(self, tournament, view_round_no, round=None):
+        self.tournament = tournament
+        self.round = round
+        self.view_round_no = view_round_no
+
+    def display(self):
+        for idx, each in (enumerate(self.matches, 1)):
+            print("Match " + idx)
+            print(each["players"][0] + " vs " + each["players"][1])
+            if(each["completed"]):
+                if(each["winner"] == None):
+                    print("The match concluded with no winner.")
+                else:
+                    print("The match concluded with " + each["winner"] + "as the winner.")
+            else:
+                print("The match is ongoing.")
+
+    def get_command(self):
+        """Gets the command for this screen."""
+        while True:
+            print("Currently Viewing Round " + self.view_round_no)
+            value = self.input_string()
+            if value.upper() == "B":
+                return NoopCmd("tournament-view", tournament=self.tournament)
+            # elif value.isdigit() and (self.view_round_no == (self.tournament.current_round - 0)):
