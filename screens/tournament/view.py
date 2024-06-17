5b89375b8f2318d80e12e26f36088f1a596f9966
aFloatingEcho <60680403+aFloatingEcho@users.noreply.github.com>
2024-06-16 22:29:05 -0700

    added tournament scoring

--- screens/tournament/view.py
+++ screens/tournament/view.py
@@ -15,28 +15,38 @@ class TournamentView(BaseScreen):
         print("Venue:", self.tournament.venue)
         print("Number of Rounds:", self.tournament.number_of_rounds)
         print("Current Round:", self.tournament.current_round)
-        print("Round Information:")
         if (self.tournament.current_round == None):
             print("Tournament has finished.")
-
         else:
-            print("Current Round Information:")
-            for idx, each in enumerate(self.tournament.rounds[self.tournament.current_round - 1], 1):
-                print("Match " + str(idx))
-                print(each["players"][0] + " vs " + each["players"][1])
-                if(each["completed"]):
-                    if(each["winner"] == None):
-                        print("The match concluded with no winner.")
+            print("Tournament is ongoing.")
+
+    def get_standings(self):
+        # Function to obtain the current standings. Note that this is like extremely inefficient.
+        standings = {}
+        for each in self.tournament.players:
+            standings[each] = 0.0
+        for each_round in self.tournament.rounds:
+            for each_match in each_round:
+                if(each_match['completed']):
+                    if(each_match['winner'] is not None):
+                        standings.update({each_match['winner']: standings.get(each_match['winner']) + 1.0}) 
                     else:
-                        print("The match concluded with " + each["winner"] + "as the winner.")
-                else:
-                    print("The match is ongoing.")
+                        standings.update({each_match['players'][0] : standings.get(each_match['players'][0]) + .5})
+                        standings.update({each_match['players'][1] : standings.get(each_match['players'][1]) + .5})
+        # This particular line is due to this: https://www.freecodecamp.org/news/sort-dictionary-by-value-in-python/
+        sorted_standings = sorted(standings.items(), key=lambda x:x[1], reverse=True)
+        standings = dict(sorted_standings)
+        return standings
 
     def get_command(self):
         """Gets the command for this screen"""
         while True:
+            current_standings = self.get_standings()
+            print("The tournament standings are as follows:")
+            for key, value in current_standings.items():
+                print(f"{key}: {value}")
             print("Viewing tournament. Type 'B' to go back.")
-            if(self.tournament.current_round != None):
+            if(self.tournament.current_round is not None):
                 print("Press 'E' to edit the current round.")
             value = self.input_string()
             if value.upper() == "B":
