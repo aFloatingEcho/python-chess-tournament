d530ba83b140906927f5b18231224d10f060bd4a
aFloatingEcho <60680403+aFloatingEcho@users.noreply.github.com>
2024-06-16 20:14:54 -0700

    continued work on view fix

--- screens/matches/view.py
+++ screens/matches/view.py
@@ -6,27 +6,24 @@ from ..base_screen import BaseScreen
 class MatchView(BaseScreen):
     """Screen displayed when viewing a Match."""
 
-    def __init__(self, players, completed, winner):
-        self.players = players
-        self.completed = completed
-        self.winner = winner
+    def __init__(self, tournament, match):
+        self.tournament = tournament
+        self.match = match
 
     def display(self):
-        print(str(self))
-        if (self.winner is not None):
-            print("The match has a winner!")
-            print("The winner is: ", self.winner)
-        else:
-            print("The match currently has no winner.")
-        if (self.completed):
+        print(str(self.match))
+        if (self.match['completed']):
             print("The match is concluded.")
+            if (self.match.get('winner') is None):
+                print("The match ended in a stalemate.")
+            else:
+                print("The match has a winner!")
+                print("The winner is: ", self.match['winner'])
         else:
             print("The match is ongoing.")
 
     def get_command(self):
         while True:
             print("Current match in progress:")
-            self.display(self)
-            action = self.input_string()
-            if action.upper() == "E":
-                return NoopCmd("club-view", club=self.club)
+            self.display()
+            return NoopCmd("tournament-view", tournament=self.tournament)
